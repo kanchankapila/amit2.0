cc1c894ee699ae5d87572b660d65e6a899c6d939
Amit Kapila <amit.kapila.2009@gmail.com>
2023-03-18 12:27:10 +0530

    cmt

--- netlify/functions/trendlynedvmwrite/trendlynedvmwrite.js
+++ netlify/functions/trendlynedvmwrite/trendlynedvmwrite.js
@@ -1,48 +1,36 @@
+const axios = require('axios');
 const { MongoClient } = require('mongodb');
-const client1 = new MongoClient(process.env.MONGODB_ATLAS_CLUSTER_URI, { useUnifiedTopology: true });
 
-const fetch = require('node-fetch')
+exports.handler = async (event) => {
+  const uri = 'mongodb+srv://amit:amit0605@cluster0.mxilo.mongodb.net/?retryWrites=true&w=majority';
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  await client.connect();
 
-exports.handler = async (event,context,callback) => {
-   const tlid = (event.body);
-  try {
-    // await client1.connect();
-    console.log("tlid"+event.body)
-     console.log(event.body)
-    // const promises = args.map(async symbol => {
-    
-        
-    // const response= axios.get('https://trendlyne.com/mapp/v1/stock/web/ohlc/' + symbol.tlid).then((response) => {
-    //     obj1=({ Date: symbol.Date,Time:symbol.time, Name: symbol.name,DurabilityScore: response.data.body.stockData[6], VolatilityScore: response.data.body.stockData[7], MomentumScore: response.data.body.stockData[8]  })
-        
-    //   })
-  
-  //   try {
-  //     await Promise.all(promises)
-  //   } catch (e) {
-  //     console.log(e)
-  //   }
-  // })
-  
-    if (!response.ok) {
-      return { statusCode: response.status, body: response.statusText };
-    }
-    const data = await response.json();
-    console.log(data)
-    let compressedData = JSON.stringify({ data });
-    compressedData = compressedData.replace(/\s/g, ""); // this line removes whitespace 
-    process.env.trendlynedvm = compressedData;
-    return {
-      statusCode: 200,
-      body: process.env.trendlynedvm,
+  const db = client.db('<DATABASE_NAME>');
+  const collection = db.collection('<COLLECTION_NAME>');
+
+  const { objects } = JSON.parse(event.body);
+
+  const results = [];
+
+  for (const obj of objects) {
+    const url = `https://example.com/${obj.param}`;
+
+    const response = await axios.get(url);
+
+    const data = {
+      ...obj,
+      output: response.data,
     };
-  } catch (error) {
-    console.error(error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ msg: error.message }),
-    };
-  } finally {
-      await client1.close();
+
+    const result = await collection.insertOne(data);
+    results.push(result);
   }
+
+  await client.close();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ results }),
+  };
 };
