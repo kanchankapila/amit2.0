6b0905477cc799fdb98e80e127c3036fc9fbb1b3
Amitkapila1 <amit.kapila.2009@gmail.com>
2023-03-20 21:11:16 +0530

    commit

--- netlify/functions/trendlyne3/trendlyne3.js
+++ netlify/functions/trendlyne3/trendlyne3.js
@@ -1,27 +1,59 @@
 const fetch = require('node-fetch')
 const { MongoClient } = require('mongodb');
 const client = new MongoClient(process.env.MONGODB_ATLAS_CLUSTER_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-   
+const axios=require('axios');   
 const trendlyne = async (tlid,event, context,callback) => {
-  
+  const obj=[];
   try {
     
     // const uri = 'mongodb+srv://amit:amit0605@cluster0.mxilo.mongodb.net/?retryWrites=true&w=majority';
-    await client.connect();
+    // await client.connect();
   
-    const db = client.db('DVM');
-    const collection = db.collection('DVM');
-    
-    url=JSON.parse(tlid)
-  
+    // const db = client.db('DVM')
+    // const collection=db.collection('DVM');
+    // console.log("Client Connected...")
+     url=JSON.parse(tlid)
+     const start = Date.now();
     const results=await Promise.all(url.map(async  url => {
       try{
         // console.log(url.tlid)
         const response = await fetch('https://trendlyne.com/mapp/v1/stock/chart-data/' + url.tlid + '/SMA/?format=json', {
           headers: { Accept: 'application/json' },
-        })
+         
+        }).then(async (response) => {
         const data1 = await response.json()
-        console.log(data1.body['stockData'])
+        obj.push({ Date: url.Date,Time:url.time, Name: url.name,DurabilityScore: data1.body['stockData'][6], VolatilityScore: data1.body['stockData'][7], MomentumScore: data1.body['stockData'][8]  })
+        }).catch((error) => {
+          console.log(error)
+        })
+        // console.log(data1.body['stockData'])
+        console.log(obj)
+        const data2 = {
+          "collection": "DVM",
+          "database": "DVM",
+          "dataSource": "Cluster0",
+          "filter":{},
+          "update":{$set: {
+            "output": obj,
+            
+            "time": start
+          }},
+          "upsert":true
+          };
+          const config = {
+            method: 'post',
+            url: 'https://data.mongodb-api.com/app/data-cibaq/endpoint/data/v1/action/updateOne',
+            headers: {
+              'Content-Type': 'application/json',
+              'Access-Control-Request-Headers': '*',
+              'api-key': 'hhsIfhonChu0fJ000k04e1k7nb5bX1CvkIWLw17FRjrzLg7kWihbY7Sy4UUKwoUy',
+              'Accept': 'application/ejson'
+            },
+            data2,
+        };
+        const result = await axios(config);
+     
+    // await collection.insertMany({"output":obj});
         return{url,data: data1};
       } catch(error){
         return{url,error:error.message}
@@ -35,13 +67,10 @@ const trendlyne = async (tlid,event, context,callback) => {
     }
    
    
-    const data = {
-      ...obj,
-      output: data1,
-    };
+    
 
-    const result = await collection.insertOne(data1);
-    results.push(result);
+  
+    
   
 
   
@@ -60,7 +89,9 @@ const trendlyne = async (tlid,event, context,callback) => {
     }
   }
    finally{
-    await client.close();
+   
+    // await client.close();
+    console.log("Client disonnected...")
   }
 }
 const handler = async (event) => {
