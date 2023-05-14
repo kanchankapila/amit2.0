6c127e46a24806c6c0866e98d48158a2a9d235cb
Amitkapila1 <amit.kapila.2009@gmail.com>
2023-05-14 15:11:00 +0530

    cmt

---
+++ netlify/functions/planetscaletest/planetscaletest.js
@@ -0,0 +1,29 @@
+require('dotenv').config('/.env')
+const mysql = require('mysql2')
+const connection = mysql.createConnection(process.env.DATABASE_URL);
+
+const handler = async function () {
+    try{
+    connection.connect();
+    console.log("connected Successfully to PlanetScale DB")
+    connection.query('SELECT * FROM mc', function (err, rows, fields) {
+        if (err) throw err
+    
+        res.send(rows)
+      })
+    connection.end()
+    return {
+        statusCode: 200,
+        body: JSON.stringify({}),
+      }
+    } catch (error) {
+      console.error(error)
+      return {
+        statusCode: 500,
+        headers: {
+          "Access-Control-Allow-Origin": "*", // Allow from anywhere 
+      },
+        body: JSON.stringify({ msg: error.message }),
+      }
+}}
+module.exports = { handler };
