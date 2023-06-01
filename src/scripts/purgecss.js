b12d7c1d46da3a5f60e49f952bc79bd3e804d538
Amit Kapila <amit.kapila.2009@gmail.com>
2023-06-01 11:26:22 +0530

    cmt

---
+++ src/scripts/purgecss.js
@@ -0,0 +1,45 @@
+const exec = require('child_process').exec;
+const fs = require('fs');
+const path = require('path');
+
+// find the styles css file
+const files = getFilesFromPath('./dist/amitnginx/', '.css');
+let data = [];
+
+if (!files && files.length <= 0) {
+console.log('cannot find style files to purge');
+return;
+}
+
+for (let f of files) {
+// get original file size
+const originalSize = getFilesizeInKiloBytes('./dist/amitnginx/' + f) + 'kb';
+var o = { file: f, originalSize: originalSize, newSize: '' };
+data.push(o);
+}
+console.log('Run PurgeCSS...');
+exec(
+'./node_modules/purgecss/bin/purgecss.js -css dist/amitnginx/*.css --content dist/amitnginx/index.html dist/amitnginx/*.js -o dist/amitnginx/',
+function (error, stdout, stderr) {
+console.log('PurgeCSS done');
+console.log();
+
+for (let d of data) {
+// get new file size
+const newSize = getFilesizeInKiloBytes('./dist/amitnginx/' + d.file) + 'kb';
+d.newSize = newSize;
+}
+console.table(data);
+}
+);
+
+function getFilesizeInKiloBytes(filename) {
+var stats = fs.statSync(filename);
+var fileSizeInBytes = stats.size / 1024;
+return fileSizeInBytes.toFixed(2);
+}
+
+function getFilesFromPath(dir, extension) {
+let files = fs.readdirSync(dir);
+return files.filter((e) => path.extname(e).toLowerCase() === extension);
+}
