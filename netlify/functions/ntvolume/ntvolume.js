1df0e12f245bc5b6d6f6ade637bf350b090d00e7
Amitkapila1 <amit.kapila.2009@gmail.com>
2023-06-08 14:49:43 +0530

    cmt

--- netlify/functions/ntvolume/ntvolume.js
+++ netlify/functions/ntvolume/ntvolume.js
@@ -1,5 +1,5 @@
 const { Client } = require('pg');
-
+const fetch = require('node-fetch')
 exports.handler = async (event, context) => {
   const client = new Client({ connectionString: process.env.POSTGRESS_DATABASE_URL1 });
 
