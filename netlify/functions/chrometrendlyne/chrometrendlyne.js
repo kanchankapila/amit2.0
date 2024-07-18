c498b44c5a4fae6a7cd2fcd87238f01155aef5e8
kanchankapila <127405216+kanchankapila@users.noreply.github.com>
2024-07-18 20:59:58 +0530

    cmt

--- netlify/functions/chrometrendlyne/chrometrendlyne.js
+++ netlify/functions/chrometrendlyne/chrometrendlyne.js
@@ -1,72 +1,29 @@
 const puppeteer = require("puppeteer-core");
 const chromium = require("@sparticuz/chromium");
 
+module.exports = {
+  handler: async () => {
+    try {
+      const browser = await puppeteer.launch({
+        args: chromium.args,
+        defaultViewport: chromium.defaultViewport,
+        executablePath: await chromium.executablePath(),
+        headless: chromium.headless,
+        ignoreHTTPSErrors: true,
+      });
 
-(async () => {
-  let browser = null;
-  console.log('Spawning Chrome headless');
+      const page = await browser.newPage();
 
-  try {
-    const start = Date.now();
-     const executablePath = await  process.env.CHROME_PATH
+      await page.goto("https://www.google.com", { waitUntil: "networkidle0" });
 
-    browser = await puppeteer.launch({
-      args: chromium.args,
-      defaultViewport: chromium.defaultViewport,
-      executablePath:executablePath  ,
-      headless: chromium.headless,
-      ignoreHTTPSErrors: true,
-    });
+      console.log("Chromium:", await browser.version());
+      console.log("Page Title:", await page.title());
 
-    const page = await browser.newPage();
-    await page.setCacheEnabled(true);
+      await page.close();
 
-    const targetUrl = 'https://trendlyne.com/visitor/loginmodal/';
-    await page.goto(targetUrl, { waitUntil: 'domcontentloaded' });
-
-    await page.type('#id_login', process.env.TRENDLYNE_EMAIL);
-    await page.type('#id_password', process.env.TRENDLYNE_PASSWORD);
-    
-    const cookies = await page.cookies();
-
-    let trnd = '';
-    let csrf = '';
-
-    for (let cookie of cookies) {
-      if (cookie.name === '.trendlyne') {
-        trnd = cookie.value;
-      }
-      if (cookie.name === 'csrftoken') {
-        csrf = cookie.value;
-      }
-    }
-
-    console.log(`Trendlyne cookie: ${trnd}`);
-    console.log(`CSRF token: ${csrf}`);
-
-    // await axios.post('https://ap-south-1.aws.data.mongodb-api.com/app/data-lhekmvb/endpoint/data/v1/action', {
-    //   collection: 'cookie',
-    //   database: 'Trendlynecookie',
-    //   dataSource: 'Cluster0',
-    //   filter: {},
-    //   update: {
-    //     $set: {
-    //       csrf: csrf,
-    //       trnd: trnd,
-    //       time: start
-    //     }
-    //   },
-    //   upsert: true
-    // });
-
-    const timeTaken = Date.now() - start;
-    console.log(`Total time taken: ${timeTaken} milliseconds`);
-
-  } catch (error) {
-    console.error(error);
-  } finally {
-    if (browser) {
       await browser.close();
+    } catch (error) {
+      throw new Error(error.message);
     }
-  }
-})();
+  },
+};
