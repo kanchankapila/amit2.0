659b2777b3e4e71709d073c97327721d203b2503
kanchankapila <127405216+kanchankapila@users.noreply.github.com>
2024-07-18 21:11:56 +0530

    cmt

--- netlify/functions/chrometrendlyne/chrometrendlyne.js
+++ netlify/functions/chrometrendlyne/chrometrendlyne.js
@@ -1,13 +1,13 @@
 const puppeteer = require("puppeteer-core");
 const chromium = require("@sparticuz/chromium");
-
+const executablePath = await chromium.executablePath() || process.env.CHROME_PATH
 module.exports = {
   handler: async () => {
     try {
       const browser = await puppeteer.launch({
         args: chromium.args,
         defaultViewport: chromium.defaultViewport,
-        executablePath: await chromium.executablePath(),
+        executablePath: executablePath,
         headless: chromium.headless,
         ignoreHTTPSErrors: true,
       });
