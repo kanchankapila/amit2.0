443a8a31f274720796eeb652bff96246375e1d02
kanchankapila <127405216+kanchankapila@users.noreply.github.com>
2024-07-26 14:54:58 +0530

    cmt

--- netlify/functions/tlrefresh/tlrefresh.js
+++ netlify/functions/tlrefresh/tlrefresh.js
@@ -1,101 +1,53 @@
-const chromium = require('@sparticuz/chromium')
-const puppeteer = require('puppeteer-core')
-const axios = require('axios');
+const chromium = require("@sparticuz/chromium");
+const puppeteer = require('puppeteer-core');
 
-exports.handler = async (event,context,callback) => {
-  
-    let browser = null
-    console.log('spawning chrome headless')
-    try {
-      const start = Date.now();
-      const executablePath = process.env.CHROME_EXECUTABLE_PATH || await chromium.executablePath 
- 
-      // setup
-      browser = await puppeteer.launch({
-             args: chromium.args,
-           
-        executablePath: executablePath,
-         headless:chromium.headless,
-          ignoreHTTPSErrors: true,
-            // ignoreDefaultArgs: ["--disable-extensions","--single-process"]
-      })
- 
-      // Use page cache when loading page.
-      page = await browser.newPage();
-      await page.setCacheEnabled(true)
-      
-      const targetUrl = 'https://trendlyne.com/visitor/loginmodal/'
- 
-      await page.goto(targetUrl, {
-        waitUntil: ["domcontentloaded"]
-      })
- 
-     
-         await page.type('#id_login', 'amit.kapila.2009@gmail.com');
-         
-         await page.type('#id_password', process.env.TRENDLYNE_PASSWORD);
-       
-          
-    cookie = await page.cookies()
-    // console.log(cookie)
-    for (let val in cookie){
-     
-        if (cookie[val].name == '.trendlyne'){
-          process.env.trnd=cookie[val].value
-         console.log( process.env.trnd)
-       }}
-       for (let val in cookie){
-       if (cookie[val].name == 'csrftoken'){
-         process.env.csrf=cookie[val].value
-        console.log( process.env.csrf)
-      }
-    }
-   
-      const data = {
-        "collection": "cookie",
-        "database": "Trendlynecookie",
-        "dataSource": "Cluster0",
-        "filter":{},
-        "update":{$set: {
-          "csrf":  process.env.csrf,
-          "trnd":  process.env.trnd,
-          "time": start
-        }},
-        "upsert":true
-        };
-        const config = {
-          method: 'post',
-          url: 'https://ap-south-1.aws.data.mongodb-api.com/app/data-lhekmvb/endpoint/data/v1/action/updateOne',
-          headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Request-Headers': '*',
-            'api-key': process.env.mongoapikey,
-            'Accept': 'application/ejson'
-          },
-          data,
-      };
-      const result = await axios(config);
-            
-      // return response data
-      callback(null, {
-          statusCode: 200,
-          body: "Inserted"
-      });
-      const timeTaken = Date.now() - start;
-      console.log(`Total time taken: ${timeTaken} milliseconds`);
- 
-     
-    } catch (error) {
-      console.log(error);
-      callback(err);
-      return {
-        statusCode: 500,
-        body: JSON.stringify({ msg: error.message }),
-      };
-    } finally {
-      if (browser) {
-          await browser.close();
-        // await client.close();
-      }
-    }
+chromium.setHeadlessMode = true; 
+chromium.setGraphicsMode = false;
+
+exports.handler = async function (event, context) {
+  try{
+  const browser = await puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath("/opt/build/repo/node_modules/chromium/lib/chromium/chrome-linux/chrome"),
+    headless: chromium.headless,
+  });
+   // Do stuff with headless chrome
+   const page = await browser.newPage()
+   const targetUrl = 'https://davidwells.io'
+
+   // Goto page and then do stuff
+   await page.goto(targetUrl, {
+     waitUntil: ["domcontentloaded", "networkidle0"]
+   })
+
+   await page.waitForSelector('#phenomic')
+
+   theTitle = await page.title();
+
+   console.log('done on page', theTitle)
+
+ } catch (error) {
+   console.log('error', error)
+   return {
+     statusCode: 500,
+     body: JSON.stringify({
+       error: error
+     })
+   }
+ } finally {
+   // close browser
+  //  if (browser !== null) {
+  //    await browser.close()
+  //  }
+ }
+
+
+
+return {
+    statusCode: 200,
+    body: JSON.stringify({
+        status: "The 'Element' function works!"        
+    })
   };
+}
