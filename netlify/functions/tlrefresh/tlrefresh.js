9b0ee4c7f9eaa209ad35a4a9d069118ea88a8aa5
kanchankapila <127405216+kanchankapila@users.noreply.github.com>
2024-08-22 01:03:46 +0530

    cmt

--- netlify/functions/tlrefresh/tlrefresh.js
+++ netlify/functions/tlrefresh/tlrefresh.js
@@ -1,4 +1,4 @@
-const chromium = require('@sparticuz/chromium');
+const chromium = require('@sparticuz/chromium-min');
 const puppeteer = require('puppeteer-core');
 const axios = require('axios');
 require('dotenv').config();
