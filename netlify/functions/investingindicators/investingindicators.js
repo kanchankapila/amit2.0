9b4dc93d4d1c904ca4d34d01c522e61b8fb88db2
kanchankapila <127405216+kanchankapila@users.noreply.github.com>
2024-08-28 13:05:47 +0530

    commit

--- netlify/functions/investingindicators/investingindicators.js
+++ netlify/functions/investingindicators/investingindicators.js
@@ -3,6 +3,7 @@ const investingindicators = async (indexid, duration) => {
     const response = await fetch(`https://api.investing.com/api/financialdata/technical/analysis/${indexid}/${duration}`, {
       headers: {
         "accept": "*/*",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36",
         "accept-language": "en-US,en;q=0.9",
         "content-type": "application/json",
         "domain-id": "in",
